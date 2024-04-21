c4c5059d271a360a50e8612287aa82d1801996e1
hiuhihi78 <leduchieu2001x@gmail.com>
2024-04-21 16:22:26 +0700

    feat: add AddUserCommandHandler

---
+++ src/MeChat.Application/UseCases/V1/User/CommandHandlers/AddUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MeChat.Common.Abstractions.Messages;
+using MeChat.Common.Shared.Response;
+using MeChat.Common.UseCases.V1.User;
+using MeChat.Domain.Abstractions.Dapper.Repositories;
+using MeChat.Domain.Abstractions.EntityFramework;
+using MeChat.Domain.Abstractions.EntityFramework.Repositories;
+
+namespace MeChat.Application.UseCases.V1.User.CommandHandlers;
+public class AddUserCommandHandler : ICommandHandler<Command.AddUser>
+{
+    private readonly IRepositoryBase<Domain.Entities.User, Guid> userRepository;
+    private readonly IUnitOfWork unitOfWork;
+    private readonly IMapper mapper;
+
+    public AddUserCommandHandler(IRepositoryBase<Domain.Entities.User, Guid> userRepository, IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        this.userRepository = userRepository;
+        this.unitOfWork = unitOfWork;
+        this.mapper = mapper;
+    }
+
+    public async Task<Result> Handle(Command.AddUser request, CancellationToken cancellationToken)
+    {
+        var user = mapper.Map<Domain.Entities.User>(request);
+
+        userRepository.Add(user);
+
+        await unitOfWork.CommitAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
