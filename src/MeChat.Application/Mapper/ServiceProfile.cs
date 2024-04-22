f7b2b71858c6fcee6029b89476b64d8ba17c6267
hiuhihi78 <leduchieu2001x@gmail.com>
2024-04-22 19:04:23 +0700

    fix: mapping class Response.User with User

--- src/MeChat.Application/Mapper/ServiceProfile.cs
+++ src/MeChat.Application/Mapper/ServiceProfile.cs
@@ -7,8 +7,13 @@ public class ServiceProfile : Profile
     public ServiceProfile()
     {
         #region V1
+
+        #region User
         CreateMap<User, Common.UseCases.V1.User.Command.AddUser>().ReverseMap();
         CreateMap<User, Common.UseCases.V1.User.Command.UpdateUser>().ReverseMap();
+        CreateMap<User, Common.UseCases.V1.User.Response.User>().ReverseMap();
+        #endregion
+
         #endregion
 
         #region V2
