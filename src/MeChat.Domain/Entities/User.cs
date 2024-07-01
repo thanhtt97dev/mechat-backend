f18a3c09fede66226b2730eaf99c96579c3cede8
hiuhihi78 <leduchieu2001x@gmail.com>
2024-07-02 00:15:38 +0700

    Update DB: add new field "Fullname" of User

--- src/MeChat.Domain/Entities/User.cs
+++ src/MeChat.Domain/Entities/User.cs
@@ -5,6 +5,7 @@ public class User : DomainEntity<Guid>
 {
     public string? Username { get; set; }
     public string? Password { get; set; }
+    public string? Fullname { get; set; }
     public int RoldeId { get; set; }
     public string? Email { get; set; }
     public string? Avatar { get; set; }
---
+++ src/MeChat.Persistence/Migrations/20240701165019_ModifileUser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeChat.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class ModifileUser : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "Fullname",
+            table: "User",
+            type: "nvarchar(max)",
+            nullable: true);
+
+        migrationBuilder.UpdateData(
+            table: "Role",
+            keyColumn: "Id",
+            keyValue: 1,
+            columns: new[] { "DateCreated", "DateUpdated" },
+            values: new object[] { new DateTime(2024, 7, 1, 23, 50, 18, 980, DateTimeKind.Local).AddTicks(5199), new DateTime(2024, 7, 1, 23, 50, 18, 980, DateTimeKind.Local).AddTicks(5241) });
+
+        migrationBuilder.UpdateData(
+            table: "Role",
+            keyColumn: "Id",
+            keyValue: 2,
+            columns: new[] { "DateCreated", "DateUpdated" },
+            values: new object[] { new DateTime(2024, 7, 1, 23, 50, 18, 980, DateTimeKind.Local).AddTicks(5244), new DateTime(2024, 7, 1, 23, 50, 18, 980, DateTimeKind.Local).AddTicks(5244) });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Fullname",
+            table: "User");
+
+        migrationBuilder.UpdateData(
+            table: "Role",
+            keyColumn: "Id",
+            keyValue: 1,
+            columns: new[] { "DateCreated", "DateUpdated" },
+            values: new object[] { new DateTime(2024, 4, 27, 11, 52, 7, 638, DateTimeKind.Local).AddTicks(2591), new DateTime(2024, 4, 27, 11, 52, 7, 638, DateTimeKind.Local).AddTicks(2602) });
+
+        migrationBuilder.UpdateData(
+            table: "Role",
+            keyColumn: "Id",
+            keyValue: 2,
+            columns: new[] { "DateCreated", "DateUpdated" },
+            values: new object[] { new DateTime(2024, 4, 27, 11, 52, 7, 638, DateTimeKind.Local).AddTicks(2603), new DateTime(2024, 4, 27, 11, 52, 7, 638, DateTimeKind.Local).AddTicks(2604) });
+    }
+}
