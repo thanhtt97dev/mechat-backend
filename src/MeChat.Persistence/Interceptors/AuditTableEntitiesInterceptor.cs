aabd8e98caa24220af7b2a910a62f09932d27e09
Le Duc Hieu <85660078+hiuhihi78@users.noreply.github.com>
2024-07-29 15:01:29 +0700

    Fix save change intercrepor for user tracking

--- src/MeChat.Persistence/Interceptors/AuditTableEntitiesInterceptor.cs
+++ src/MeChat.Persistence/Interceptors/AuditTableEntitiesInterceptor.cs
@@ -1,4 +1,4 @@
-﻿using MeChat.Domain.Abstractions.Enitites;
+using MeChat.Domain.Abstractions.Enitites;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
@@ -15,11 +15,20 @@ public class AuditTableEntitiesInterceptor : SaveChangesInterceptor
             return base.SavingChangesAsync(eventData, result, cancellationToken);
 
         IEnumerable<EntityEntry<IAuditTable>> entries = dbContext.ChangeTracker.Entries<IAuditTable>();
-        if(entries.Count() == 0) return base.SavingChangesAsync(eventData, result, cancellationToken);
+        if(entries.Count() == 0) 
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+
+        if(entries.Any
+            (x => x.State == EntityState.Deleted ||
+             x.State == EntityState.Modified ||
+             x.State == EntityState.Added)
+            && UserTrackingAditTableHelper.UserId == null)
+        {
+            throw new Exception("Must be save changes data with user tracking");
+        }
 
         bool isUserTracking = true;
-        var userId = UserTrackingAditTableHelper.UserId;
-        if (userId == null) isUserTracking = false;
+        if (UserTrackingAditTableHelper.UserId == null) isUserTracking = false;
 
         foreach (var entityEntry in entries)
         {
@@ -32,13 +41,13 @@ public class AuditTableEntitiesInterceptor : SaveChangesInterceptor
             if(entityEntry.State == EntityState.Modified)
             {
                 if (isUserTracking)
-                    entityEntry.Property(nameof(IAuditTable.ModifiedBy)).CurrentValue = userId;
+                    entityEntry.Property(nameof(IAuditTable.ModifiedBy)).CurrentValue = UserTrackingAditTableHelper.UserId;
                 entityEntry.Property(nameof(IAuditTable.ModifiledDate)).CurrentValue = DateTimeOffset.Now;
             }
             if (entityEntry.State == EntityState.Added)
             {
                 if (isUserTracking)
-                    entityEntry.Property(nameof(IAuditTable.CreatedBy)).CurrentValue = userId;
+                    entityEntry.Property(nameof(IAuditTable.CreatedBy)).CurrentValue = UserTrackingAditTableHelper.UserId;
                 entityEntry.Property(nameof(IAuditTable.CreatedDate)).CurrentValue = DateTimeOffset.Now;
             }
         }
