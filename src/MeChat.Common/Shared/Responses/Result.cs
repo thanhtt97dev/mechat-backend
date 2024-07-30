2dad6ff3e9a6eb02473628dc70dd5213606dacad
Le Duc Hieu <85660078+hiuhihi78@users.noreply.github.com>
2024-07-30 16:47:52 +0700

    add new constructor for Result

--- src/MeChat.Common/Shared/Responses/Result.cs
+++ src/MeChat.Common/Shared/Responses/Result.cs
@@ -1,4 +1,4 @@
-﻿using MeChat.Common.Constants;
+using MeChat.Common.Constants;
 using System.Text.Json.Serialization;
 
 namespace MeChat.Common.Shared.Response;
@@ -23,47 +23,73 @@ public class Result
         Ok = ok;
     }
 
+    #region Success
     public static Result Success()
-        =>new (AppConstants.ResponseCodes.Success, "Success!", true);
+        => new(AppConstants.ResponseCodes.Success, "Success!", true);
 
     public static Result<TData> Success<TData>(TData data)
         => new(AppConstants.ResponseCodes.Success, "Success!", true, data);
+    #endregion
 
-    public static Result Failure(string message) 
+    #region Failure
+    public static Result Failure(string message)
         => new(AppConstants.ResponseCodes.Failure, message, false);
-
+    public static Result<TData> Failure<TData>(string message)
+    => new(AppConstants.ResponseCodes.Failure, message, false);
     public static Result<TData> Failure<TData>(TData? data, string message)
         => new(AppConstants.ResponseCodes.Failure, message, false, data);
+    #endregion
 
+    #region NotFound
     public static Result<string> NotFound(string message)
         => new(AppConstants.ResponseCodes.NotFound, message, false, null);
 
+    public static Result<TData> NotFound<TData>(string message)
+        => new(AppConstants.ResponseCodes.NotFound, message, false);
+
     public static Result<TData> NotFound<TData>(TData? data, string message)
         => new(AppConstants.ResponseCodes.NotFound, message, false, data);
+    #endregion
 
+    #region UnAuthorized
     public static Result<string> UnAuthorized(string message)
         => new(AppConstants.ResponseCodes.UnAuthorized, message, false, null);
 
+    public static Result<TData> UnAuthorized<TData>(string message)
+        => new(AppConstants.ResponseCodes.UnAuthorized, message, false);
+
     public static Result<TData> UnAuthorized<TData>(TData? data, string message)
         => new(AppConstants.ResponseCodes.UnAuthorized, message, false, data);
+    #endregion
 
+    #region UnAuthentication
     public static Result<string> UnAuthentication(string message)
         => new(AppConstants.ResponseCodes.UnAuthentication, message, false, null);
 
+    public static Result<TData> UnAuthentication<TData>(string message)
+        => new(AppConstants.ResponseCodes.UnAuthentication, message, false);
+
     public static Result<TData> UnAuthentication<TData>(TData? data, string message)
         => new(AppConstants.ResponseCodes.UnAuthentication, message, false, data);
+    #endregion
 
+    #region Initialization
     public static Result Initialization(string code, string message, bool oke)
-        => new(code,message, oke);
+        => new(code, message, oke);
+
+    public static Result<TData> Initialization<TData>(string code, string message, bool oke)
+        => new(code, message, oke);
 
     public static Result<TData> Initialization<TData>(string code, string message, bool oke, TData? data)
         => new(code, message, oke, data);
+    #endregion
 
+    #region ValidationError
     public static Result<TData> ValidationError<TData>(TData data)
     {
         return new(AppConstants.ResponseCodes.ValidationError, "Validation error!", false, data);
     }
-
+    #endregion
 }
 
 public class Result<TValue> : Result
@@ -75,5 +101,10 @@ public class Result<TValue> : Result
          : base(code, message, oke)
     {
         this.Value = value;
+    }
+
+    public Result(string? code, string? message, bool ok, object? value = null) : base(code, message, ok)
+    {
+        if (value is not null) throw new Exception("Invalid constructor");
     }
 }
