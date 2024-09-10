5d3e234cfa6f60659db1a5e941230e299461b85c
hiuhihi78 <leduchieu2001x@gmail.com>
2024-09-11 00:58:53 +0700

    add validator for api update user's password

---
+++ src/MeChat.Common/UseCases/V1/User/Validators/UpdateUserPasswordValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace MeChat.Common.UseCases.V1.User.Validators;
+public class UpdateUserPasswordValidator: AbstractValidator<Command.UpdateUserPassword>
+{
+    public UpdateUserPasswordValidator()
+    {
+        RuleFor(x => x.Username).NotEmpty();
+    }
+}
