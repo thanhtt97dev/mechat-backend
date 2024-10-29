2a1c4be4a077d733355aea402ea9ad073c60d316
hiuhihi78 <leduchieu2001x@gmail.com>
2024-10-29 21:52:48 +0700

    addition migration db files

---
+++ src/MeChat.Persistence/Migrations/20241029143136_ModifyNotificationEntity_v3.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeChat.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class ModifyNotificationEntity_v3 : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Link",
+            table: "Notification");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "Link",
+            table: "Notification",
+            type: "nvarchar(max)",
+            nullable: true);
+    }
+}
