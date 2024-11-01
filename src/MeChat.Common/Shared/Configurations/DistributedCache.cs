89767871c12bf6dd2eac213be9c8727f3845485e
hiuhihi78 <leduchieu2001x@gmail.com>
2024-11-01 23:59:40 +0700

    Update appsettings.json and source config for distributed cache

---
+++ src/MeChat.Common/Shared/Configurations/DistributedCache.cs
@@ -0,0 +1,17 @@
+﻿namespace MeChat.Common.Shared.Configurations;
+public sealed class DistributedCache
+{
+    public string Mode { get; set; } = nameof(DistributedCache.InMemory);
+    public InMemory InMemory { get; set; } = new();
+    public Redis Redis { get; set; } = new();
+}
+
+public sealed class InMemory
+{
+    
+}
+
+public sealed class Redis
+{
+    public string ConnectionStrings { get; set; } = string.Empty;
+}
---
+++ src/MeChat.Infrastucture.DistributedCache/Services/InMemoryCacheService.cs
@@ -0,0 +1,37 @@
+﻿using MeChat.Common.Abstractions.Services;
+using Microsoft.Extensions.Caching.Memory;
+using System.Runtime.InteropServices;
+
+namespace MeChat.Infrastucture.DistributedCache.Services;
+public class InMemoryCacheService : ICacheService
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public InMemoryCacheService(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    public Task<string?> GetCache(string key)
+    {
+        _memoryCache.TryGetValue(key, out string? value);
+        return Task.FromResult(value);
+    }
+
+    public Task RemoveCache(string key)
+    {
+        _memoryCache.Remove(key);
+        return Task.CompletedTask;
+    }
+
+    public Task SetCache(string key, object value, [Optional] TimeSpan timeOut)
+    {
+        var cacheEntryOptions = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = timeOut == TimeSpan.Zero ? TimeSpan.FromMinutes(30) : timeOut
+        };
+
+        _memoryCache.Set(key, value, cacheEntryOptions);
+        return Task.CompletedTask;
+    }
+}
